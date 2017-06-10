260e04f406223d2f7d9f8967913e7203a60edaff
Mr.Chen <ian2018cs@gmail.com>
2017-06-10 21:48:22 +0800

    添加几个工具类，更新土司工具，方法参数去掉上下文环境

---
+++ app/src/main/java/com/example/administrator/my/utils/Constant.java
@@ -0,0 +1,12 @@
+package com.example.administrator.my.utils;
+
+/**
+ * 存储常量值
+ */
+
+public class Constant {
+    /**
+     * sharedPreferences名字
+     */
+    public static String SHAREDPREFERENCES_NAME = "config";
+}
--- app/src/main/java/com/example/administrator/my/utils/ToastUtil.java
+++ app/src/main/java/com/example/administrator/my/utils/ToastUtil.java
@@ -1,17 +1,17 @@
 package com.example.administrator.my.utils;
 
-import android.content.Context;
 import android.widget.Toast;
+
+import com.example.administrator.my.MyApplication;
 
 /**
  * 土司工具
  */
 public class ToastUtil {
 	/**
-	 * @param ctx	上下文环境
 	 * @param msg	打印文本内容
 	 */
-	public static void show(Context ctx,String msg) {
-		Toast.makeText(ctx, msg,Toast.LENGTH_SHORT).show();
+	public static void show(String msg) {
+		Toast.makeText(MyApplication.getContext(), msg,Toast.LENGTH_SHORT).show();
 	}
 }
