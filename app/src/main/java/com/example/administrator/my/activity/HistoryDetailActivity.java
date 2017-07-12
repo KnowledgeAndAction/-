0e8517120aaaab160eca21172b07060626cf9440
mookcc <cgz950301>
2017-07-12 18:01:07 +0800

    调整优化历史详情页面布局

--- app/src/main/java/com/example/administrator/my/activity/HistoryDetailActivity.java
+++ app/src/main/java/com/example/administrator/my/activity/HistoryDetailActivity.java
@@ -17,6 +17,7 @@ public class HistoryDetailActivity extends AppCompatActivity {
     private TextView tv_hintime;
     private TextView tv_houttime;
     private TextView tv_hname;
+    private TextView tv_hlocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +33,13 @@ public class HistoryDetailActivity extends AppCompatActivity {
         tv_hintime = (TextView) findViewById(R.id.tv_hintime);
         tv_houttime = (TextView) findViewById(R.id.tv_houttime);
         tv_hname = (TextView) findViewById(R.id.tv_hname);
+        tv_hlocation= (TextView) findViewById(R.id.tv_hlocation);
 
-        tv_details.setText(historyActivity.getActivityDescription());
+        tv_details.setText("  "+historyActivity.getActivityDescription());
         tv_hintime.setText(historyActivity.gethInTime().replace("T"," "));
-        tv_houttime.setText(historyActivity.gethOutTime().replace("T",""));
+        tv_houttime.setText(historyActivity.gethOutTime().replace("T"," "));
         tv_hname.setText(historyActivity.gethActivityName());
+        tv_hlocation.setText(historyActivity.getLocation());
 
     }
 
